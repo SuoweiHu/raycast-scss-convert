be21c62c76c5439323b7fdd32c9675d25661f615
Suowei Hu <suowei.h@gmail.com>
2024-06-15 10:39:27 +1000

    Feat: move "sass compiler path" and "option to del before compile" into preference (and add placeholder for watch compile cmd)

---
+++ src/cmd_watchCompile.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Action, ActionPanel, List } from "@raycast/api";
+
+const items = ["Augustiner Helles", "Camden Hells", "Leffe Blonde", "Sierra Nevada IPA"];
+
+export default function Command() {
+  const [searchText, setSearchText] = useState("");
+
+  return (
+    <List
+      searchText={searchText}
+      onSearchTextChange={setSearchText}
+      navigationTitle="Search Beers"
+      searchBarPlaceholder="Search your favorite beer"
+    >
+      {items.map((item) => (
+        <List.Item
+          key={item}
+          title={item}
+          actions={
+            <ActionPanel>
+              <Action title="Select" onAction={() => setSearchText(item)} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
