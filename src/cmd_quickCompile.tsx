324342dfea031fcb06f4c073a4595793759e6130
Suowei Hu <suowei.h@gmail.com>
2024-06-16 12:56:53 +1000

    Lint: resolve error found in "npm run lint"

--- src/cmd_quickCompile.tsx
+++ src/cmd_quickCompile.tsx
@@ -2,10 +2,5 @@ import { CompilForm } from "./cmp_CompileForm";
 import { QuickCompileAction } from "./cmp_QuickCompileAction";
 
 export default function Command() {
-    return (
-        <CompilForm
-            FormAction={QuickCompileAction}
-            show_watchOption={false}
-            restore_prevConfig={true} />
-    );
+  return <CompilForm FormAction={QuickCompileAction} show_watchOption={false} restore_prevConfig={true} />;
 }
